de5119ac88132c45aa91c87db5c8cbd4f42bfefb
Фатеев Владислав <FaVl@gendalf.ru>
2016-11-08 05:19:24 +0300

    Web API Testing

---
+++ AdminServer/Controllers/SnifferController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AdminServer.Controllers
+{
+    [RoutePrefix("sniffer")]
+    public class SnifferController : ApiController
+    {
+        
+        [HttpGet]
+        [ActionName("test")]
+        public string Test()
+        {
+            return "OK";
+        }
+
+    }
+}
