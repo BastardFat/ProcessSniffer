42167bc8e47ac6245563f6fe36c8aa7550885a4a
Фатеев Владислав <FaVl@gendalf.ru>
2016-11-08 06:51:23 +0300

    Implement minimal config management Implement Basic Authentication for API

--- AdminServer/Controllers/SnifferController.cs
+++ AdminServer/Controllers/SnifferController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdminServer.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,9 +14,10 @@ namespace AdminServer.Controllers
         
         [HttpGet]
         [ActionName("test")]
-        public string Test()
+        [BasicAuthentication]
+        public IHttpActionResult Test()
         {
-            return "OK";
+            return Ok("cool");
         }
 
     }
