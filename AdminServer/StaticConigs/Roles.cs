3748749570649754a92a1d9e5d9aabb2e63de753
Фатеев Владислав <FaVl@gendalf.ru>
2016-11-10 22:23:58 +0300

    Dynamical adding to config list using JQuery

---
+++ AdminServer/StaticConigs/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AdminServer.Models;
+
+namespace AdminServer.StaticConigs
+{
+    public static class Roles
+    {
+        public static List<Models.Role> AllRoles = GetRoles();
+
+        private static List<Role> GetRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
